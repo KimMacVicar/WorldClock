c648f136e42d49008b7bbd222edd1b01da7c2c78
KimMacVicar <92276525+KimMacVicar@users.noreply.github.com>
2022-11-26 21:59:48 -0600

    added link to return to homepage

--- javascript/index.js
+++ javascript/index.js
@@ -57,7 +57,7 @@ function updateCity(event) {
   <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}
     </small></div>
     </div>
-
+<a href="index.html">Return to Homepage</a>
   `;
 }
 
